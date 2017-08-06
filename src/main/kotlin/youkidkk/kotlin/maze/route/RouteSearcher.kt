54a6bb48fb5beef295fc3fa125ebc9858eaea604
youkidkk <youkidkk80@gmail.com>
2017-08-06 09:47:20 +0900

    コメントを追加

--- src/main/kotlin/youkidkk/kotlin/maze/route/RouteSearcher.kt
+++ src/main/kotlin/youkidkk/kotlin/maze/route/RouteSearcher.kt
@@ -3,10 +3,23 @@ package youkidkk.kotlin.maze.route
 import youkidkk.kotlin.maze.Maze
 import youkidkk.kotlin.maze.Point
 
+/**
+ * ルート探索を行うためのクラス。
+ *
+ * @param maze 迷路
+ * @param start 開始地点
+ * @param end 終了地点
+ */
 class RouteSearcher(val maze: Maze, val start: Point, val end: Point) {
 
+    /**
+     * 到達済み地点マップ。
+     */
     lateinit var visited: MutableMap<Point, List<Point>>
 
+    /**
+     * ルート探索を行う。
+     */
     fun search() : List<Point> {
         visited = mutableMapOf()
         val route = mutableListOf<Point>()
@@ -19,13 +32,25 @@ class RouteSearcher(val maze: Maze, val start: Point, val end: Point) {
         return visited.get(end)!!
     }
 
+    /**
+     * 対象地点から再帰的にルートを探索する。
+     *
+     * @param point 対象地点
+     * @param route これまでのルート
+     */
     private fun go(point: Point, route: List<Point>) {
+        // 対象地点が終了地点の場合は処理を終了する
         if (point == end) {
             return
         }
+
+        // 移動可能な方向リストで繰り返し
         val directions = maze.getMoveableDirections(point)
         for (direction in directions) {
+            // 移動先地点
             val distPoint = point + (direction.pointInc * 2)
+
+            // 移動先地点が既に到達済みでない場合、再帰的にルートを探索する
             if (!visited.containsKey(distPoint)) {
                 val next = mutableListOf<Point>()
                 next.addAll(route)
